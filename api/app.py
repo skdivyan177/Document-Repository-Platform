b3b6dbc1c41db5747554e3aac41ebf65bec0e3a5
ddivyan117 <137909253+skdivyan177@users.noreply.github.com>
2026-06-02 11:45:50 -0700

    Changes made, now working pipeline and Flask

--- api/app.py
+++ api/app.py
@@ -1,13 +1,36 @@
-from Flask import Flask
-from database import init_db
+from flask import Flask, request
+from database import init_db, insert_document, get_all_documents
+import os
 
 app = Flask(__name__)
+init_db()
 
 @app.route("/")
-def func():
+def health():
     return {
         "status": "running",
         "service": "document repository platform"
+    }
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    file = request.files["file"]
+    save_path = os.path.join("uploads", file.filename)
+    file.save(save_path)
+    file_size = os.path.getsize(save_path)
+
+    insert_document(file.filename, file_size)
+    
+    return {
+        "message": "uploaded successfully",
+        "filename": file.filename
+    }
+
+@app.route("/documents")
+def documents(): 
+    documents = get_all_documents()
+    return {
+        "documents" : documents
     }
 
 if __name__ == "__main__":
--- api/database.py
+++ api/database.py
@@ -1,10 +1,9 @@
 import sqlite3
+from datetime import datetime
 
 def init_db():
     conn = sqlite3.connect("documents.db")
-
     cursor = conn.cursor()
-
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS documents (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,6 +13,34 @@ def init_db():
         upload_date TEXT
     )
     """)
-
     conn.commit()
     conn.close()
+
+def insert_document(filename, file_size):
+    conn = sqlite3.connect("documents.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   INSERT INTO documents (filename, page_count, 
+                                        file_size, upload_date)
+                     VALUES (?, ?, ?, ?)""", 
+                    (filename, 0, file_size, datetime.now().isoformat()))
+    conn.commit()
+    conn.close()
+
+def get_all_documents():
+
+    conn = sqlite3.connect("documents.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT *
+    FROM documents
+    """)
+
+    documents = cursor.fetchall()
+
+    conn.close()
+
+    return documents
